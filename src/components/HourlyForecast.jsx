cfdc1be612cf0045193bee5f903fec092af76b9c
gigachadou <axmedovaasira@gmail.com>
2026-02-07 22:23:31 +0500

    Main: Commit before publication

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,14 +1,20 @@
 import getWeatherIcon from "../../utils/getWeatherIcon"
 
 export default function HourlyForecast({ data }) {
+    function detectNight(time) {
+        if (time == "-") return false;
+        const hour = Number(time.slice(0, 2))
+        return (hour > 20 || hour < 7)
+    };
     return (
         <div className="hourly__container">
             <h5>Hourly forecast</h5>
             {data.data.map((e, i) => {
+                const time = e.time?.split("T")[1]?.slice(0, 5) || "—"
                 return (
                     <div className="hourly__div" key={i}>
-                        {getWeatherIcon(e.weatherCode, 36, "orange")}
-                        <h5>{e.time?.split("T")[1]?.slice(0, 5) || "—"}</h5>
+                        {getWeatherIcon(e.weatherCode, 36, "orange", detectNight(time))}
+                        <h5>{time}</h5>
                         <h5>{e.temp + " " + data.units.temperature}</h5>
                     </div>
                 )
